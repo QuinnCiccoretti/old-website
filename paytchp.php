61be777e990ea1761d35328d8a8e82cbd4a6079d
Ciccoretti <2018qciccore@local.tjhsst.edu>
2016-10-12 12:51:35 -0400

    make it better

---
+++ paytchp.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>PHP tho</title>
+</head>
+<body>
+	<button id = "doIt"></button>
+	<div id = "output"></div>
+	<?php
+		echo("<input id = 'quinnput'> Harambe died for our sins </input>");
+
+	?>
+</body>
+</html>
+
+
