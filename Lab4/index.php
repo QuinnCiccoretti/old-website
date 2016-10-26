90af5914737d4a30186d25456e7edfb977f0031d
Ciccoretti <2018qciccore@local.tjhsst.edu>
2016-10-26 13:07:20 -0400

    make it better

--- Lab4/index.php
+++ Lab4/index.php
@@ -8,23 +8,18 @@
 			xhttp.onreadystatechange = function(){
 				if((this.readyState==4)&&(this.status==200)){
 					console.log(this.responseText);
-					// words = this.responseText.split(" ").slice(0,10).join();
-					// done[input]=words;
-					// document.getElementById("Output").innerHTML = words;
+					
 				}
 			}
-			// if(!(input in done)){
+			
 			 	xhttp.open("Get","https://lit-app.herokuapp.com/words.php?query="+input,true)
 			 	xhttp.send(input);
-			// }
-			// else{
-			// 	document.getElementById("Output").innerHTML = done[input];
-			// }
+			
 		}
 	</script>
 </head>
 <body>
 	First name: <input type="text" id="qInput"><br>
-
+	<button type = "button" onclick = "getWord()">Click</button>
 	</body>
 </html>
