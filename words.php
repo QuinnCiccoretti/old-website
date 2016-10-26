93088c606afbeeb7df8b4e5788015f2d2a29a82c
Ciccoretti <2018qciccore@local.tjhsst.edu>
2016-10-26 13:31:20 -0400

    make it better

--- words.php
+++ words.php
@@ -1,6 +1,13 @@
-<?php
+<html>
+<head>
+	<title>Words</title>
+</head>
+<body>
+	<?php
 		// $words = file_get_contents(words.txt);
 		 $wordIn = $_REQUEST["wordIn"];
 		echo $wordIn;
 		
 ?>
+</body>
+</html>
