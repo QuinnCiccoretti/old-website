166707626691bffe59fdac1ead0ef54ed6c2934f
Ciccoretti <2018qciccore@local.tjhsst.edu>
2016-10-26 13:19:28 -0400

    make it better

--- words.php
+++ words.php
@@ -1,13 +1,6 @@
 <?php
-		$myfile = fopen("words.txt", "r") or die("Unable to open file!");
-		$words = array ();
-		// Output one line until end-of-file
-		while(!feof($myfile)) {
-		   
-		   $words[fgets($myfile) . ""]= 1;
-		}
-		fclose($myfile);
+		$words = file_get_contents(words.txt);
 		$wordIn = $_REQUEST["wordIn"];
-		echo "" . array_key_exists($wordIn,$words)
+		echo $wordIn . $words;
 		
 ?>
